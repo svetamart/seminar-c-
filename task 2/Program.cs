997d15e1200cabd473cd18e2c1a1fb5bb2863526
Sveta Martynova <svetamart95@gmail.com>
2022-08-21 21:26:12 +0300

    added new task

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
