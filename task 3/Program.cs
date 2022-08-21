47f938d022d148d6afb42aaccaecac3b50b4cfe4
Sveta Martynova <svetamart95@gmail.com>
2022-08-21 21:53:41 +0300

    modified task 3

--- task 3/Program.cs
+++ task 3/Program.cs
@@ -4,8 +4,10 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int negative_number = number * -1; // определяем границу диапозона - то же число, но со знаком минус 
 
-while negative_number <= number // пока наше отрицательное число меньше или равно положительному 
+while (negative_number <= number) // пока наше отрицательное число меньше или равно положительному 
 {
+     
     Console.Write(negative_number); // выводим это число в строку
+    Console.Write(" ");
     negative_number++; // то же самое, что negative number = negative_number + 1(??)
 }
