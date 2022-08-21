c4391be1a68d3962ebb9117af98eeba496ae7637
Sveta Martynova <svetamart95@gmail.com>
2022-08-21 20:46:21 +0300

    added task 1

---
+++ task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите число a");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число b");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b * b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
