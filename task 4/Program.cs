b7fbbce56b885adbe2049a1230e8fb8bbb817b21
Sveta Martynova <svetamart95@gmail.com>
2022-08-21 22:02:13 +0300

    added task 4 (answer)

--- task 4/Program.cs
+++ task 4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number % 10; // остаток от деления. если делить трехзначное число на 10, то после запятой как раз получится последняя цифра
+Console.WriteLine(number2);
